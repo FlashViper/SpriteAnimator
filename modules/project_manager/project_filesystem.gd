6f4d697a228ea9afa4dbd9fa6e2f4e03bf24d6a2
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-05 15:59:59 -0600

    Added supports_image method

--- modules/project_manager/project_filesystem.gd
+++ modules/project_manager/project_filesystem.gd
@@ -14,12 +14,10 @@ var extensions : Array[String] = [
 func get_files_from_dir(path: String) -> Dictionary:
 	var result := {}
 	var open : Array[String] = [""]
-	
 	while open.size() > 0:
 		var current_path : String = open.pop_front()
 		var files := PackedStringArray()
 		
-		print("%s/%s" % [path, current_path])
 		var dir := DirAccess.open("%s/%s" % [path, current_path])
 		dir.list_dir_begin()
 		
@@ -68,3 +66,7 @@ func add_recent_path(new: String) -> void:
 	for n in new_paths:
 		if n:
 			f.store_line(n)
+
+
+func supports_image(extension: String) -> bool:
+	return extensions.has(extension)
