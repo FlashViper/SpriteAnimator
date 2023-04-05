955504f1325859a706bfcf973b0278cc5a71bcd0
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-05 16:23:27 -0600

    Added comments to texture packer
    Hopefully this will help future me
    I procrastinated doing math 302 homework for this, so it better

--- modules/texture_packer/texture_packer.gd
+++ modules/texture_packer/texture_packer.gd
@@ -6,29 +6,36 @@ func pack_textures(textures: Array[Texture2D], parameters := {}) -> Dictionary:
 		printerr("Tried to pass a null array to pack_textures in texture_packer.gd")
 		return {}
 	
+	# First, crop all of the sprites by their alpha
+	# so we are left with the least empty space possible
 	var frames : Array[SpriteRegion] = crop_to_frame(textures)
 	
+	# Assemble a list of the cropped regions of the textures
 	var rects : Array[Rect2i] = []
 	for f in frames:
 		rects.append(f.region)
 	
+	# Pack the rects using out magical packer function
 	var packed := pack_rects(rects, parameters.get("padding", 2))
 	var data : Array[Dictionary] = []
 	
+	# Assemble some data about each of the textures
 	for i in frames.size():
 		var frame_data := frames[i].get_data()
-		frame_data["source_texture"] = textures[i]
-		frame_data["atlas_region"] = packed["result"][i]
+		frame_data["source_texture"] = textures[i] # Texture we are pulling from
+		frame_data["atlas_region"] = packed["result"][i] # Region on the final atlas
 		
 		data.append(frame_data)
-		
 		frames[i].region = packed.result[i]
 	
+	# Use our magical texture assembly function to pack all of the
+	# textures together
 	var tex := assemble_texture(data, packed.width, packed.height)
 	
+	# Return our result!
 	return {
-		"frames": data,
-		"texture": tex
+		"frames": data, # The individual frames, including their atlas position and their pivot
+		"texture": tex  # The final assembled texture
 	}
 
 
@@ -46,30 +53,38 @@ func crop_to_frame(textures : Array[Texture2D]) -> Array[SpriteRegion]:
 
 
 func pack_rects(rects: Array[Rect2i], padding := 0) -> Dictionary:
+	# Store the original order of the rects so we can preserve their connections
 	var remapped : Array[int] = []
 	for i in rects.size():
 		remapped.append(i)
 	
-	var placed := {}
-	var availible_points : Array[Vector2i] = [Vector2i.ZERO]
-	var max_x := 0
-	var max_y := 0
-	
-	# sort in order of ascending area
+	# sort the rects by order of ascending area (largest come first)
 	remapped.sort_custom(func(a: int, b: int): return rects[a].get_area() < rects[b].get_area())
 	
+	var placed := {} # Dictionary linking index in rects to final location
+	var availible_points : Array[Vector2i] = [Vector2i.ZERO] # Points availible to place a rect
+	var max_x := 0 # Current atlas size (x)
+	var max_y := 0 # Current atlas size (y)
+	
 	for i in remapped.size():
+		# Get the rect with the next most area
+		# Create a new rect that we can try to place
 		var index : int = remapped.pop_back()
-		var rect : Rect2i = rects[index] # get the rect with the next most area
+		var rect : Rect2i = rects[index]
 		var placed_rect := Rect2i(Vector2i(), rect.size)
-		var min_cost := INF
+		var min_cost := INF # The lowest cost we can find for this iteration
 		
+		# Loop through the points that we have determined can be placed at
 		for n in availible_points.size():
+			# Grab the next availible point and find the new atlas size
 			var a := availible_points[n]
 			var new_max_x := maxi(max_x, a.x + rect.size.x)
 			var new_max_y := maxi(max_y, a.y + rect.size.y)
+			
+			# Compute the cost (length of the size vector)
 			var cost := Vector2(new_max_x, new_max_y).length_squared()
 			
+			# If the cost is less than the current lowest cost, replace it
 			if cost < min_cost:
 				var overlaps := false
 				for idx in placed:
@@ -83,29 +98,37 @@ func pack_rects(rects: Array[Rect2i], padding := 0) -> Dictionary:
 				placed_rect.position = a
 				min_cost = cost
 		
+		# Update the new size values
 		max_x = maxi(max_x, placed_rect.end.x)
 		max_y = maxi(max_y, placed_rect.end.y)
 		
-		# do I even need this? probably?
-		# turn on if you ever see problems with this script
+		# To save on memory, maybe?, remove all
+		# availible points that lie inside the placed rect
+		# ---do I even need this? probably?
+		# ---turn on if you ever see problems with this script
 		var expanded := placed_rect.grow(padding)
 		for ii in range(availible_points.size(), 0, -1):
 			if expanded.has_point(availible_points[ii - 1]):
 				availible_points.remove_at(ii - 1)
-		availible_points.shuffle()
 		
+		
+		# To the availible points, add...
+		#	- The bottom-right corner of the rect
+		#	- The top-right corner of the rect
+		#	- The bottom-left corner of the rect
 		availible_points.append(placed_rect.end + Vector2i.ONE * padding)
 		availible_points.append(Vector2i(placed_rect.position.x, placed_rect.end.y) + Vector2i(0, padding))
 		availible_points.append(Vector2i(placed_rect.end.x, placed_rect.position.y) + Vector2i(padding, 0))
 		availible_points.append(Vector2i(max_x, max_y))
 		placed[index] = placed_rect
-#		result.append(placed_rect)
-#		result[index] = placed_rect
 	
+	# Create a new array by translating back from the mapping
+	# into the correct indicies
 	var result : Array[Rect2i] = rects.duplicate()
 	for i in placed:
 		result[i] = placed[i]
 	
+	# Return a dictionary with key details about the calculations
 	return {
 		"result": result,
 		"width": max_x,
@@ -113,6 +136,10 @@ func pack_rects(rects: Array[Rect2i], padding := 0) -> Dictionary:
 	}
 
 
+# Pretty self-explanatory. Given a texture, returns a Rect that
+# represents the minimum region of the texture that conains an image
+# If that sounds confusing, use your intuition for cropping
+# Or just write a simple test script :)
 func crop_rect(texture: Texture2D, alpha_threshold := 0.2, padding := 0) -> Rect2i:
 	var result := Rect2i(Vector2i(), texture.get_size())
 	var img := texture.get_image()
@@ -166,6 +193,7 @@ func crop_rect(texture: Texture2D, alpha_threshold := 0.2, padding := 0) -> Rect
 
 
 func assemble_texture(data: Array[Dictionary], width: int, height: int) -> Texture2D:
+	# Create an image with the required specs
 	var img := Image.create(width, height, false, Image.FORMAT_RGBA8)
 	
 	for d in data:
@@ -173,6 +201,11 @@ func assemble_texture(data: Array[Dictionary], width: int, height: int) -> Textu
 		var source_region := d.source_region as Rect2i
 		var atlas_region := d.atlas_region as Rect2i
 		
+		# Lifesaving function that speeds up image creation immensly
+		# Directly copies the image data from the source to the final image
+		# Skips doing a super nasty double for loop, which is slow even outside
+		# of GDScript, where it takes up almost as much time as packing the rects,
+		# sometimes even more
 		img.blit_rect(source.get_image(), source_region, atlas_region.position)
 	
 
