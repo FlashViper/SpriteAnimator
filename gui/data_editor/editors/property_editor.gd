a2ec3aee69e48c9f8f326e2081b330134153dd90
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-15 15:10:57 -0600

    Added property editor base class
    (for use with collision and stuff)

---
+++ gui/data_editor/editors/property_editor.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var canvas : Control
+
+
+func initialize(p_canvas: Control) -> void:
+	if canvas:
+		canvas.gui_input.disconnect(on_canvas_input)
+		canvas.draw.disconnect(draw)
+	
+	canvas = p_canvas
+	canvas.draw.connect(draw)
+	canvas.gui_input.connect(on_canvas_input)
+
+
+func on_canvas_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				_on_mouse_down(event)
+			else:
+				_on_mouse_up(event)
+	
+	if event is InputEventMouseMotion:
+		_on_mouse_drag(event)
+
+
+func draw() -> void:
+	if canvas:
+		_draw_controls()
+
+
+func _on_mouse_down(event: InputEventMouseButton) -> void: pass
+func _on_mouse_up(event: InputEventMouseButton) -> void: pass
+func _on_mouse_drag(event: InputEventMouseMotion) -> void: pass
+func _draw_controls() -> void: pass
